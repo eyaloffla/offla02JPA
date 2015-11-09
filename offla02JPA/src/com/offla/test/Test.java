9557a34e7c6fb07699fc79bc284cc84c25cdc98b
eyal <eyal.r.offla@gmail.com>
2015-11-09 05:16:01 +0200

    first commit

---
+++ offla02JPA/src/com/offla/test/Test.java
@@ -0,0 +1,20 @@
+package com.offla.test;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Test
+ */
+@Stateless
+@LocalBean
+public class Test {
+
+    /**
+     * Default constructor. 
+     */
+    public Test() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
